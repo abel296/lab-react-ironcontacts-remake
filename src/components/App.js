304e968a752ca8c97422ca85b41199ee29e677ba
Abel Andrés <abelandres.gtc@gmail.com>
2023-02-10 17:13:24 +0100

    ✨ add functionality new random contact

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { ContactList } from './ContactsList';
 import contacts from '../contacts.json';
 import './App.css'
 
 
 export function App() {
-  const [contactsState, setContactsState] = useState()
+  const [contactsState, setContactsState] = useState(contacts.filter((elm, idx) => idx <= 4))
   
-  const firstFiveContacts = contacts.filter((elm, idx) => idx <= 4)
-  useEffect(() => setTimeout(() => setContactsState(firstFiveContacts), 1000), [firstFiveContacts])
+  function addNewRandomContact(contacts) {
+    const restContacts = contacts.filter(elm => !contactsState.includes(elm))
+    const randomIndex = Math.floor(Math.random() * ((restContacts.length - 1) - 0) + 0)
+    setContactsState([...contactsState, restContacts[randomIndex]])
+  }
 
   return <>
     <section>
       <h1>IronContacts</h1>
+      <button onClick={ () => addNewRandomContact(contacts) }>Add New Random Contact</button>
       { contactsState ? <ContactList contacts={ contactsState }></ContactList> : <i>Cargando</i> }
     </section>
   </>
